3028365693223c85b0db8d1c86cc2ea16ad99f89
Irving Han <irving.han@knx.com.cn>
2023-12-06 11:51:07 +0800

    feat: readonly & deep-readonly

---
+++ deep-readonly/index.ts
@@ -0,0 +1,9 @@
+type DeepReadonly<T> = T extends Object
+  ? T extends [infer A, ...infer B]
+    ? readonly [DeepReadonly<A>, ...DeepReadonly<B>]
+    : T extends Function
+    ? T
+    : {
+        readonly [P in keyof T]: DeepReadonly<T[P]>;
+      }
+  : T;
